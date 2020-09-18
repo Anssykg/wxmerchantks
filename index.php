18f0d2a87f80a2c17be204c5a2f6c29f904392ea
1099815904@qq.com <1099815904@qq.com>
2020-09-18 17:30:51 +0800

    111

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @Created by 666.
+ * @User: yuankaiguo
+ * @Date: 2020/9/18
+ * @Time: 11:36
+ */
+
+require_once "vendor/autoload.php";
+
+class TestClass{
+    public function index(){
+        $obj = new WxMerChantKs\Test();
+        var_dump($obj->index());
+    }
+}
+
+(new TestClass())->index();
---
+++ src/Test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @Created by 666.
+ * @User: yuankaiguo
+ * @Date: 2020/9/18
+ * @Time: 11:35
+ */
+
+namespace WxMerChantKs;
+
+class Test
+{
+    public function index(){
+        return 12;
+    }
+}
